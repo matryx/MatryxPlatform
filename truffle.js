86b810963dbd0fcfb377a85a9528589536a68808
Max Howard <tmhoward@ucsd.edu>
2017-11-20 18:05:49 -0800

    fixed truffle.js

--- truffle.js
+++ truffle.js
@@ -2,10 +2,11 @@ module.exports = {
   networks: {
     development: {
       host: "localhost",
-      port: 4551,
+      port: 8545,
       network_id: "*", // Match any network id
-      //gas: 2900000,
-      //gasPrice: 18000000000
+      gas: 2900000,
+      gasPrice: 18000000000,
+      from: '0x11f2915576dc51dffb246959258e8fe5a1913161'
     },
     test: {
       host: "localhost",
