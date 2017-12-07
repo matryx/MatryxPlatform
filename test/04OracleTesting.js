8c6ba8ed44814663fd0e10ce560ba00039aa62ce
Max Howard <tmhoward@ucsd.edu>
2017-12-07 15:29:39 -0800

    Changed the name of 01InitialTesting\.js to 01PlatformTesting\.js

--- test/04OracleTesting.js
+++ test/04OracleTesting.js
@@ -13,5 +13,23 @@ contract('MatryxPlatform', function(accounts)
 	// 	});
 	// });
 
-	
+	// it("The platform triggers a TournamentCreated event per tournament.", function()
+	// {
+	// 	return MatryxAlpha.deployed().then(function(instance)
+	// 	{
+	// 		platform = instance;
+	// 		var balanceQueryEvent = platform.QueryPerformed({_from:web3.eth.coinbase},{fromBlock: 0, toBlock: 'latest'});
+	// 		return platform.createTournament("new tournament", "external address", 100, 1)
+	// 		.then(() => 
+	// 			{
+	// 				// Additionally you can start watching right away, by passing a callback:
+	// 				web3.eth.filter('latest', function(error, result){
+	// 					if (!error)
+	// 					{
+	// 					  	console.log(result);
+	// 					}
+	// 				});
+	// 			});
+	// 	});
+	// });
 });
