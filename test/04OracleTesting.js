bfdda46c80c4e2e46840a5e3f0b8f44eb7f24cae
Max Howard <tmhoward@ucsd.edu>
2017-12-07 13:54:42 -0800

    renamed a couple files

---
+++ test/04OracleTesting.js
@@ -0,0 +1,17 @@
+var utils = require("./utils.js");
+var MatryxAlpha = artifacts.require("MatryxPlatform");
+
+contract('MatryxPlatform', function(accounts)
+{
+
+	// it("The oracle system produces a valid query.", function()
+	// {
+	// 	return MatryxAlpha.deployed().then(function(instance)
+	// 	{
+	// 		platform = instance;
+	// 		return platform.prepareBalance.call(accounts[0]);
+	// 	});
+	// });
+
+	
+});
