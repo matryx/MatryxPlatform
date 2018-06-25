753a1af72e2b6cc57cc3fd2efb780613e038797b
Marina Torras <mtorras@ucsd.edu>
2018-06-25 16:03:01 -0700

    modifying platform tests to use structs

--- test/02PlatformTesting.js
+++ test/02PlatformTesting.js
@@ -10,6 +10,7 @@ contract('MatryxPlatform', function(accounts){
   let tournament;
   let token;
   let submissionAddress;
+  let tournamentAddress;
   //for code coverage
   let gasEstimate = 30000000;
 
@@ -17,114 +18,132 @@ contract('MatryxPlatform', function(accounts){
   //let gasEstimate = 3000000;
 
   it("The owner of the platform should be the creator of the platform", async function() {
-      web3.eth.defaultAccount = web3.eth.accounts[0];
-      //deploy platform
-      platform = await MatryxPlatform.deployed();
+      web3.eth.defaultAccount = web3.eth.accounts[0]
+        ethers = require('/Users/marinatorras/Projects/Matryx/ethers.js'); // local ethers pull
+        wallet = new ethers.Wallet("0xb39d47dbd17325001ea80ae580d879e1e529bc5e2235cff4d1145e974d24a518")
+        wallet.provider = new ethers.providers.JsonRpcProvider('http://localhost:8545')
+        let sendEtherTxHash = web3.eth.sendTransaction({from: web3.eth.accounts[0], to: wallet.address, value: 30 * 10 ** 18})
 
-      token = web3.eth.contract(MatryxToken.abi).at(MatryxToken.address);
+        //minedTx = await getMinedTx(sendEtherTxHash, 1000);
+        //console.log("Mined Transaction: ", minedTx);
 
-      platform = web3.eth.contract(MatryxPlatform.abi).at(MatryxPlatform.address); 
-      let owner = await platform.owner();
-      let getOwner = await platform.getOwner();
-      let tournamentCount = await platform.tournamentCount();
-
-      let peerAddress = await platform.peerAddress(accounts[0]);
-
-      await platform.createPeer.sendTransaction({gas: gasEstimate});
-      peerAddress = await platform.peerAddress(accounts[0]);
-
-      //create peers
-      await platform.createPeer.sendTransaction({gas: gasEstimate, from: web3.eth.accounts[1]});
-      peerAddress = await platform.peerAddress(accounts[1]);
-      await platform.createPeer.sendTransaction({gas: gasEstimate, from: web3.eth.accounts[2]});
-      await platform.createPeer.sendTransaction({gas: gasEstimate, from: web3.eth.accounts[3]});
-      await token.setReleaseAgent(web3.eth.accounts[0]);
-
-      //release token transfer and mint tokens for the accounts
-      await token.releaseTokenTransfer.sendTransaction();
-      await token.mint(web3.eth.accounts[0], 10000*10**18)
-      await token.mint(web3.eth.accounts[1], 2*10**18)
-      await token.mint(web3.eth.accounts[2], 2*10**18)
-      await token.mint(web3.eth.accounts[3], 2*10**18)
-      await token.approve(MatryxPlatform.address, 100*10**18)
-
-      // create a tournament
-      createTournamentTransaction = await platform.createTournament("category", "tournament", "external address", 100*10**18, 2*10**18, {gas: gasEstimate});
-      tournamentCreatedEvent = platform.TournamentCreated();
-
-      tournamentCreatedEventsPromise = new Promise((resolve, reject) =>
-        tournamentCreatedEvent.get((err, res) => {
-            if (err) {
-                reject(err);
-            } else {
-                resolve(res);
+        function stringToBytes32(text, requiredLength) {
+            var data = ethers.utils.toUtf8Bytes(text);
+            var l = data.length;
+            var pad_length = 64 - (l * 2 % 64);
+            data = ethers.utils.hexlify(data);
+            data = data + "0".repeat(pad_length);
+            data = data.substring(2);
+            data = data.match(/.{1,64}/g);
+            data = data.map(v => "0x" + v);
+            while (data.length < requiredLength) {
+                data.push("0x0");
             }
-        }))
-      var tournamentsCreatedEvents = await tournamentCreatedEventsPromise;
+            return data;
+        }
 
-      //get tournament address
-      tournamentAddress = tournamentsCreatedEvents[0].args._tournamentAddress;
-      // console.log("tournamentAddress: " + tournamentAddress);
-      // create tournament from address
-      tournament = await MatryxTournament.at(tournamentAddress);
+        // funtion bytes32ArrayToString(array) {
+        //     //TODO -- Function to convert bytes32 array back into string **CHECK FOR TRAILING ZEROS**
+        // }
+
+        platform = new ethers.Contract(MatryxPlatform.address, MatryxPlatform.abi, wallet);
+
+        await platform.createPeer({gasLimit: 4000000})
+        token = new ethers.Contract(MatryxToken.address, MatryxToken.abi, wallet);
+        await token.setReleaseAgent(wallet.address)
+        await token.releaseTokenTransfer({gasLimit: 1000000})
+        await token.mint(wallet.address, "10000000000000000000000")
+        await token.approve(MatryxPlatform.address, "100000000000000000000")
+
+        title = stringToBytes32("the title of the tournament", 3);
+        categoryHash = stringToBytes32("contentHash", 2);
+
+        tournamentData = {
+            categoryHash: web3.sha3("math"),
+            title_1: title[0],
+            title_2: title[1],
+            title_3: title[2],
+            contentHash_1: categoryHash[0],
+            contentHash_2: categoryHash[1],
+            Bounty: "10000000000000000000",
+            entryFee: "2000000000000000000"
+        }
+
+        minedTime = await platform.getNow();
+        console.log("TIME OF LAST MINED BLOCK: " + minedTime.toNumber());
+
+        var endR = minedTime + 10000000000000;
+        var reviewTime = minedTime + 10000000000000;
+        console.log("minedTime: " + minedTime);
+        console.log("endR: ", endR)
+        roundData = {start: minedTime, end: endR, reviewDuration: 1000000, bounty: "5000000000000000000"}
+        await platform.createTournament("math", tournamentData, roundData, {gasLimit: 6500000})
+        tournamentAddress = await platform.allTournaments(0);
+
+        tournament = await new ethers.Contract(tournamentAddress, MatryxTournament.abi, wallet);
 
       //check who owns the tournament
-      let creatorIsOwner = await tournament.isOwner.call(accounts[0]);
-      assert(creatorIsOwner.valueOf(), true, "The owner and creator of the tournament should be the same"); 
+      let tournamentOwner = await tournament.getOwner();
+      console.log("tournamentOwner: " + tournamentOwner);
+      console.log("platform.address: " + platform.address);
+      console.log("accounts[0]: " + accounts[0]);
+      console.log("wallet.address: " + wallet.address);
+      assert.equal(tournamentOwner, wallet.address, "The owner and creator of the tournament should be the same"); 
   });
 
-  it("Tournament.openTournament should invoke a TournamentOpened event.", async function() {
+  // it("Tournament.openTournament should invoke a TournamentOpened event.", async function() {
 
-    // Start watching the platform events before we induce the one we're looking for.
-    platform.TournamentOpened().watch((error, result) => {
-        if (!error) {
-          assert.equal(result.args._tournamentName, "tournament", "The name of the tournament should be 'tournament'");
-        } else {
-          assert.false();
-        }
-    });
+  //   // Start watching the platform events before we induce the one we're looking for.
+  //   platform.TournamentOpened().watch((error, result) => {
+  //       if (!error) {
+  //         assert.equal(result.args._tournamentName, "tournament", "The name of the tournament should be 'tournament'");
+  //       } else {
+  //         assert.false();
+  //       }
+  //   });
 
-    // //get gas estimate for opening tournament
-    // gasEstimate = await tournament.openTournament.estimateGas();
-    // console.log("gasEstimate: " + gasEstimate);
-    // //gas estimate underestimates slightly for openTournament, multiply by some constant ~1.3
-    // gasEstimate = Math.ceil(gasEstimate * 1.3);
-    // console.log("gasEstimate * constant: " + gasEstimate);
+  //   // //get gas estimate for opening tournament
+  //   // gasEstimate = await tournament.openTournament.estimateGas();
+  //   // console.log("gasEstimate: " + gasEstimate);
+  //   // //gas estimate underestimates slightly for openTournament, multiply by some constant ~1.3
+  //   // gasEstimate = Math.ceil(gasEstimate * 1.3);
+  //   // console.log("gasEstimate * constant: " + gasEstimate);
 
-    //open the tournament
-    let openTournamentTx = await tournament.openTournament({gas: gasEstimate});
-  });
+  //   //open the tournament
+  //   let openTournamentTx = await tournament.openTournament({gas: gasEstimate});
+  // });
 
   it("Tournament should be mine", async function(){
-    let isTournamentMine = platform.getTournament_IsMine(tournamentAddress);
+    let isTournamentMine = platform.getTournament_IsMine(tournamentAddress, {from: wallet});
     assert.isTrue(isTournamentMine, "Tournament shoud be mine.");
   });
 
   it("Able to recognize a submission", async function(){
-    //open tournament
-    let tournamentOpen = await tournament.openTournament();
+        round_info = await tournament.currentRound();
+        round_address = round_info[1]
+        console.log(round_address)
+        //r = web3.eth.contract(MatryxRound.abi).at(tournament.rounds(0))
+        r = new ethers.Contract(round_address, MatryxRound.abi, wallet);
+        console.log("round: " + r);
 
-    // //get gas estimate for entering tournament
-    // gasEstimate = await platform.enterTournament.estimateGas(tournamentAddress);
-    // console.log("gasEstimate: " + gasEstimate);
+        let state = await tournament.getState();
+        console.log("state: " + state);
 
-    //enter tournament
-    let enteredTournament = await platform.enterTournament(tournamentAddress, {gas: gasEstimate});
+        await token.approve(MatryxPlatform.address, 0);
+        await token.approve(MatryxPlatform.address, tournamentData.entryFee);
 
-    //create and start round
-    let roundAddress = await tournament.createRound(5);
+        console.log("New Token Allowance Approved");
 
-    round = await tournament.currentRound();
-    roundAddress = round[1];
+        enter = await platform.enterTournament(tournamentAddress, {gasLimit: 6500000});
+        //enteredMinedTx = await getMinedTx(enter, 1000);
 
-    await tournament.startRound(10, 10, {gas: gasEstimate});
-    round = web3.eth.contract(MatryxRound.abi).at(roundAddress);
-
-    //open round
-    let roundOpen = await round.isOpen();
-    //create submission
-    let submissionCreated = await tournament.createSubmission("submission1", accounts[0], "external address", ["0x0"], ["0x0"], ["0x0"], {gas: gasEstimate});
-    let submissionAddress = submissionCreated.logs[0].args._submissionAddress;
+        //console.log("Entering Tournament Transaction Hash: ", enteredMinedTx);
+        console.log("Entered: ", enter);
+        submissionData = {title: "A submission", owner: web3.eth.accounts[0], contentHash: "0xabcdef1124124124124", isPublic: false}
+        await tournament.createSubmission([],[],[], submissionData, {gasLimit: 6500000});
+        console.log("Submission created");
+        let mySubmissions = await tournament.mySubmissions();
+        submissionAddress = mySubmissions[0];
 
     let isSubmission = await platform.isSubmission(submissionAddress);
     assert.isTrue(isSubmission, "Should be a submission.")
@@ -153,12 +172,9 @@ contract('MatryxPlatform', function(accounts) {
   //for regular testing
   //let gasEstimate = 3000000;
 
-  it("The number of tournaments should be 0.", function() {
-    return MatryxPlatform.deployed().then(function(instance) {
-      return instance.tournamentCount();
-    }).then(function(count) {
-      assert.equal(count.valueOf(), 0, "The tournament count was non-zero to begin with.");
-    });
+  it("The number of tournaments should be 0.", async function() {
+    let count = await platorm.tournamentCount();
+    assert.equal(count.valueOf(), 0, "The tournament count was non-zero to begin with.");
   });
 
   it("The number of tournaments should be 1", async function() {
