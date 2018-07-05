dc990368ce3769c45e2dadd12720618d4099ba12
Alex McNerney <ajm13@me.com>
2018-07-05 16:06:44 -0700

    Automatic tournament creation script

---
+++ truffle/helper.js
@@ -0,0 +1,69 @@
+const ethers = require('ethers')
+const sleep = ms => new Promise(done => setTimeout(done, ms))
+
+// key from ganache
+const key = '0x' + 'f7d0ecaecde3010efb7ffc0b0efbf98619a1f960694d52836e38a43edd88c2a7'
+
+module.exports = {
+  stringToBytes32(text, requiredLength) {
+    var data = ethers.utils.toUtf8Bytes(text)
+    var l = data.length
+    var pad_length = 64 - ((l * 2) % 64)
+    data = ethers.utils.hexlify(data)
+    data = data + '0'.repeat(pad_length)
+    data = data.substring(2)
+    data = data.match(/.{1,64}/g)
+    data = data.map(v => '0x' + v)
+    while (data.length < requiredLength) {
+      data.push('0x0')
+    }
+    return data
+  },
+
+  async setup(artifacts, web3) {
+    const MatryxPlatform = artifacts.require('MatryxPlatform')
+    const MatryxToken = artifacts.require('MatryxToken')
+    const MatryxTournament = artifacts.require('MatryxTournament')
+    const MatryxRound = artifacts.require('MatryxRound')
+    const MatryxSubmission = artifacts.require('MatryxSubmission')
+
+    const account = web3.eth.accounts[0]
+    web3.eth.defaultAccount = account
+    console.log('Account:', account)
+
+    const wallet = new ethers.Wallet(key)
+    wallet.provider = new ethers.providers.JsonRpcProvider('http://127.0.0.1:8545')
+
+    const platform = new ethers.Contract(MatryxPlatform.address, MatryxPlatform.abi, wallet)
+    const hasPeer = await platform.hasPeer(account)
+    if (!hasPeer) await platform.createPeer({ gasLimit: 4.5e6 })
+
+    const token = new ethers.Contract(MatryxToken.address, MatryxToken.abi, wallet)
+    const balance = await token.balanceOf(account) / 1e18 | 0
+    console.log('Balance: ' + balance + ' MTX')
+
+    if (balance == 0) {
+      let tokens = web3.toWei(1e5)
+      await token.setReleaseAgent(account)
+      await token.releaseTokenTransfer({ gasLimit: 1e6 })
+      await token.mint(account, tokens)
+      await token.approve(MatryxPlatform.address, tokens, { gasPrice: 21e9 })
+
+      const balance = await token.balanceOf(account) / 1e18 | 0
+      console.log('New balance: ' + balance + ' MTX')
+    }
+    console.log('setup complete\n')
+
+    return {
+      MatryxPlatform,
+      MatryxToken,
+      MatryxTournament,
+      MatryxRound,
+      MatryxSubmission,
+      account,
+      wallet,
+      platform,
+      token
+    }
+  }
+}
---
+++ truffle/test.js
@@ -0,0 +1,91 @@
+const ethers = require('ethers')
+const { setup, stringToBytes32 } = require('./helper')
+const sleep = ms => new Promise(done => setTimeout(done, ms))
+
+let MatryxTournament, MatryxRound, platform, token, wallet
+
+const genId = length => new Array(length).fill(0).map(() => Math.floor(36 * Math.random()).toString(36)).join('')
+
+const init = async () => {
+  const data = await setup(artifacts, web3)
+  MatryxTournament = data.MatryxTournament
+  MatryxRound = data.MatryxRound
+  account = data.account
+  wallet = data.wallet
+  platform = data.platform
+  token = data.token
+}
+
+const createTournament = async () => {
+  let count = +await platform.tournamentCount()
+
+  const title = stringToBytes32('Test Tournament ' + (count + 1), 3)
+  const descriptionHash = stringToBytes32('QmWmuZsJUdRdoFJYLsDBYUzm12edfW7NTv2CzAgaboj6ke', 2)
+  const fileHash = stringToBytes32('QmeNv8oumYobEWKQsu4pQJfPfdKq9fexP2nh12quGjThRT', 2)
+  const tournamentData = {
+    category: 'math',
+    title_1: title[0],
+    title_2: title[1],
+    title_3: title[2],
+    descriptionHash_1: descriptionHash[0],
+    descriptionHash_2: descriptionHash[1],
+    fileHash_1: fileHash[0],
+    fileHash_2: fileHash[1],
+    bounty: web3.toWei(10),
+    entryFee: web3.toWei(2)
+  }
+  const startTime = Math.floor(new Date() / 1000)
+  const endTime = startTime + 10
+  const roundData = {
+    start: startTime,
+    end: endTime,
+    reviewPeriodDuration: 300,
+    bounty: web3.toWei(5)
+  }
+
+  await platform.createTournament(tournamentData, roundData, { gasLimit: 8e6, gasPrice: 21e9 })
+
+  const address = await platform.allTournaments(count)
+  const tournament = new ethers.Contract(address, MatryxTournament.abi, wallet)
+  console.log('Tournament: ' + address)
+
+  return tournament
+}
+
+const createSubmission = async tournament => {
+  const isEntrant = await tournament.isEntrant(account)
+  if (!isEntrant) await platform.enterTournament(tournament.address, { gasLimit: 5e6 })
+
+  const content = stringToBytes32('QmWmuZsJUdRdoFJYLsDBYUzm12edfW7NTv2CzAgaboj6ke', 1)
+  const submissionData = {
+    title: 'A submission ' + genId(6),
+    owner: account,
+    contentHash: content[0] + content[1].substr(2),
+    isPublic: false
+  }
+  await tournament.createSubmission([], [], [], submissionData, { gasLimit: 6.5e6 })
+
+  console.log('Submission created')
+}
+
+const logSubmissions = async tournament => {
+  const rounds = await tournament.getRounds()
+  console.log('Round 1: ' + rounds[0])
+  const round = new ethers.Contract(rounds[0], MatryxRound.abi, wallet)
+  console.log(await round.getSubmissions())
+}
+
+module.exports = async exit => {
+  try {
+    await init()
+    const tournament = await createTournament()
+    await createSubmission(tournament)
+    await createSubmission(tournament)
+    await createSubmission(tournament)
+    await logSubmissions(tournament)
+  } catch (err) {
+    console.log(err.message)
+  } finally {
+    exit()
+  }
+}
