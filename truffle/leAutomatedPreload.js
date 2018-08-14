8295a23d55547c014a8991a4235e7c47ef869123
Max Howard <max@nanome.ai>
2018-08-14 13:13:30 -0700

    Updated leAutomatedPreload

--- truffle/leAutomatedPreload.js
+++ truffle/leAutomatedPreload.js
@@ -1,15 +1,18 @@
-const ethers = require('ethers')
-const { setup, stringToBytes32, stringToBytes, Contract } = require('./utils')
-const sleep = ms => new Promise(done => setTimeout(done, ms))
+const chalk = require('chalk')
 
-let MatryxTournament, MatryxRound, platform, token, wallet
+const { setup, getMinedTx, sleep, stringToBytes32, stringToBytes, Contract } = require('./utils')
+
+let MatryxTournament, MatryxRound, MatryxSubmission, platform, token, wallet
+let timeouts = []
 
 const genId = length => new Array(length).fill(0).map(() => Math.floor(36 * Math.random()).toString(36)).join('')
+const genAddress = () => '0x' + new Array(40).fill(0).map(() => Math.floor(16 * Math.random()).toString(16)).join('')
 
 const init = async () => {
   const data = await setup(artifacts, web3, 0)
   MatryxTournament = data.MatryxTournament
   MatryxRound = data.MatryxRound
+  MatryxSubmission = data.MatryxSubmission
   wallet = data.wallet
   platform = data.platform
   token = data.token
@@ -17,28 +20,28 @@ const init = async () => {
 
 const createTournament = async (bounty, roundData, accountNumber) => {
   const { platform } = await setup(artifacts, web3, accountNumber)
+
+  let account = platform.wallet.address
   let count = +await platform.tournamentCount()
 
-  console.log("Platform using account", platform.wallet.address)
+  console.log('Platform using account', platform.wallet.address)
+  console.log(`Currently ${count} Tournaments on Platform`)
 
   const suffix = ('0' + (count + 1)).substr(-2)
+  const category = stringToBytes('math')
   const title = stringToBytes32('Test Tournament ' + suffix, 3)
   const descriptionHash = stringToBytes32('QmWmuZsJUdRdoFJYLsDBYUzm12edfW7NTv2CzAgaboj6ke', 2)
   const fileHash = stringToBytes32('QmeNv8oumYobEWKQsu4pQJfPfdKq9fexP2nh12quGjThRT', 2)
   const tournamentData = {
-    category: 'math',
-    title_1: title[0],
-    title_2: title[1],
-    title_3: title[2],
-    descriptionHash_1: descriptionHash[0],
-    descriptionHash_2: descriptionHash[1],
-    fileHash_1: fileHash[0],
-    fileHash_2: fileHash[1],
+    category,
+    title,
+    descriptionHash,
+    fileHash,
     initialBounty: bounty,
     entryFee: web3.toWei(2)
   }
-//   const startTime = Math.floor(new Date() / 1000)
-//   const endTime = startTime + 60
+  //   const startTime = Math.floor(new Date() / 1000)
+  //   const endTime = startTime + 60
   // const roundData = {
   //   start: startTime,
   //   end: endTime,
@@ -46,11 +49,30 @@ const createTournament = async (bounty, roundData, accountNumber) => {
   //   bounty
   // }
 
-  await platform.createTournament(tournamentData, roundData, { gasLimit: 8e6, gasPrice: 25 })
+  let tx = await platform.createTournament(tournamentData, roundData, { gasLimit: 6e6, gasPrice: 25 })
+  await getMinedTx('Platform.createTournament', tx.hash)
 
   const address = await platform.allTournaments(count)
   const tournament = Contract(address, MatryxTournament, accountNumber)
-  console.log('Tournament: ' + address)
+  console.log(chalk`Tournament created: {green ${address}}`)
+
+  let [_, roundAddress] = await tournament.currentRound()
+  let round = Contract(roundAddress, MatryxRound, accountNumber)
+
+  console.log(chalk`Current round: {green ${roundAddress}}\n`)
+
+  let startTime = +await round.getStartTime()
+  let endTime = +await round.getEndTime()
+  let reviewEnd = endTime + roundData.reviewPeriodDuration
+
+  let now = Date.now()
+  let timeTilStart = startTime * 1000 - now
+  let timeTilEnd = endTime * 1000 - now
+  let timeTilReviewEnd = reviewEnd * 1000 - now
+
+  timeouts.push(setTimeout(() => console.log(chalk`{grey [Tournament round started]}`), timeTilStart))
+  timeouts.push(setTimeout(() => console.log(chalk`{grey [Tournament round ended]}`), timeTilEnd))
+  timeouts.push(setTimeout(() => console.log(chalk`{grey [Tournament round review ended]}`), timeTilReviewEnd))
 
   return tournament
 }
@@ -63,128 +85,248 @@ const createSubmission = async (tournament, accountNumber) => {
   const account = tournament.wallet.address
 
   const isEntrant = await tournament.isEntrant(account)
-  if (!isEntrant) await platform.enterTournament(tournament.address, { gasLimit: 5e6 })
+  if (!isEntrant) {
+    let { hash } = await platform.enterTournament(tournament.address, { gasLimit: 5e6 })
+    await getMinedTx('Platform.enterTournament', hash)
+  }
 
-  const descriptionHash = stringToBytes('QmZVK8L7nFhbL9F1Ayv5NmieWAnHDm9J1AXeHh1A3EBDqK')
-  const fileHash = stringToBytes('QmfFHfg4NEjhZYg8WWYAzzrPZrCMNDJwtnhh72rfq3ob8g')
+  const title = stringToBytes32('A submission ' + genId(6), 3)
+  const descriptionHash = stringToBytes32('QmZVK8L7nFhbL9F1Ayv5NmieWAnHDm9J1AXeHh1A3EBDqK', 2)
+  const fileHash = stringToBytes32('QmfFHfg4NEjhZYg8WWYAzzrPZrCMNDJwtnhh72rfq3ob8g', 2)
 
   const submissionData = {
-    title: 'A submission ' + genId(6),
-    owner: account,
+    title,
     descriptionHash,
     fileHash,
-    isPublic: false
+    timeSubmitted: 0,
+    timeUpdated: 0
   }
-  await tournament.createSubmission([], [], [], submissionData, { gasLimit: 6.5e6 })
 
-  console.log('Submission created')
+  const contribsAndRefs = {
+    contributors: new Array(10).fill(0).map(r => genAddress()),
+    contributorRewardDistribution: new Array(10).fill(1),
+    references: new Array(10).fill(0).map(r => genAddress())
+  }
+
+  let tx = await tournament.createSubmission(submissionData, contribsAndRefs, { gasLimit: 8e6 })
+  await getMinedTx('Tournament.createSubmission', tx.hash)
+
+  const [_, roundAddress] = await tournament.currentRound()
+  const round = Contract(roundAddress, MatryxRound)
+  const submissions = await round.getSubmissions()
+  const submissionAddress = submissions.pop()
+  const submission = Contract(submissionAddress, MatryxSubmission, accountNumber)
+
+  console.log(chalk`Submission created: {green ${submission.address}}\n`)
+  return submission
 }
 
-const getSubmissions = async tournament => {
-  const currentRoundResults = await tournament.currentRound();
-  const currentRoundAddress = currentRoundResults[1];
-  console.log('Current round: ' + currentRoundAddress)
-  const round = Contract(currentRoundAddress, MatryxRound)
+const updateSubmission = async submission => {
+  const modData = {
+    title: stringToBytes32('AAAAAA', 3),
+    descriptionHash: stringToBytes32('BBBBBB', 2),
+    fileHash: stringToBytes32('CCCCCC', 2)
+  }
+  let tx
+
+  tx = await submission.updateData(modData)
+  await getMinedTx('Submission.updateData', tx.hash)
+
+  const conModData = {
+    contributorsToAdd: new Array(3).fill(0).map(() => genAddress()),
+    contributorRewardDistribution: new Array(3).fill(1),
+    contributorsToRemove: []
+  }
+
+  tx = await submission.updateContributors(conModData)
+  await getMinedTx('Submission.updateContributors', tx.hash)
+
+  const refModData = {
+    referencesToAdd: new Array(3).fill(0).map(() => genAddress()),
+    referencesToRemove: []
+  }
+
+  tx = await submission.updateReferences(refModData)
+  await getMinedTx('Submission.updateReferences', tx.hash)
+}
+
+const logSubmissions = async tournament => {
+  const [_, roundAddress] = await tournament.currentRound();
+  console.log(chalk`Current round: {green ${roundAddress}}`)
+  const round = Contract(roundAddress, MatryxRound)
   const submissions = await round.getSubmissions()
-  console.log(submissions)
-  return submissions;
+  submissions.forEach((s, i) => {
+    console.log(chalk`Submission ${i + 1}: {green ${s}}`)
+  })
+  return submissions
+}
+
+const waitUntilClose = async (tournament) => {
+  const [_, roundAddress] = await tournament.currentRound();
+  const round = Contract(roundAddress, MatryxRound)
+  const roundEndTime = +await round.getEndTime()
+  const reviewPeriodDuration = +await round.getReviewPeriodDuration()
+  const timeTilClose = Math.max(0, roundEndTime + reviewPeriodDuration - Date.now() / 1000)
+
+  console.log(chalk`{grey [Waiting ${~~timeTilClose}s until current round over]}`)
+  await sleep(timeTilClose * 1000)
 }
 
 const selectWinnersWhenInReview = async (tournament, accountNumber, winners, rewardDistribution, roundData, selectWinnerAction) => {
   tournament.accountNumber = accountNumber
 
-  const currentRoundResults = await tournament.currentRound();
-  const roundAddress = currentRoundResults[1];
+  const [_, roundAddress] = await tournament.currentRound()
   const round = Contract(roundAddress, MatryxRound, accountNumber)
   const roundEndTime = await round.getEndTime()
 
-  var timeTilRoundInReview = roundEndTime - Date.now()/1000
-  timeTilRoundInReview = timeTilRoundInReview > 0 ? timeTilRoundInReview + 1 : 0
-  await sleep(timeTilRoundInReview*1000)
+  let timeTilRoundInReview = roundEndTime - Date.now() / 1000
+  timeTilRoundInReview = timeTilRoundInReview > 0 ? timeTilRoundInReview : 0
 
-  const params = [winners, rewardDistribution, Object.values(roundData), selectWinnerAction]
-  console.log(params)
+  console.log(chalk`{grey [Waiting ${~~timeTilRoundInReview}s until review period]}`)
+  await sleep(timeTilRoundInReview * 1000)
 
-  const res = await tournament.selectWinners(...params, {gasLimit: 5000000})
-  return res;
+  const tx = await tournament.selectWinners([winners, rewardDistribution, selectWinnerAction, 0], roundData, { gasLimit: 5000000 })
+  await getMinedTx('Tournament.selectWinners', tx.hash)
 }
 
 module.exports = async exit => {
   try {
     await init()
-    let scOne_RoundData = {
-        start: Math.floor(Date.now()/1000) + 1e9,
-        end: Math.floor(Date.now()/1000) + 1e9 + 10,
-        reviewPeriodDuration: 1,
-        bounty: web3.toWei(5)
+    const tournamentCreator = 0
+
+    let RoundNotYetOpen = {
+      start: Math.floor(Date.now() / 1000) + 9999999,
+      end: Math.floor(Date.now() / 1000) + 99999999,
+      reviewPeriodDuration: 12345678,
+      bounty: web3.toWei(3),
+      closed: false
     }
-    const tournamentOne = await createTournament(web3.toWei(10), scOne_RoundData, 0)
-    let scTwo_RoundData = {
-        start: Math.floor(Date.now()/1000),
-        end: Math.floor(Date.now()/1000) + 1e9,
-        reviewPeriodDuration: 1,
-        bounty: web3.toWei(5)
+    const tournament_a = await createTournament(web3.toWei(10), RoundNotYetOpen, tournamentCreator)
+
+    // ----------------------------------------------------------------------------------------------
+
+    let RoundUnfunded = {
+      start: Math.floor(Date.now() / 1000) + 60,
+      end: Math.floor(Date.now() / 1000) + 9999999,
+      reviewPeriodDuration: 12345678,
+      bounty: 0,
+      closed: false
     }
-    const tournamentTwo = await createTournament(web3.toWei(10), scTwo_RoundData, 0)
-    let scThree_RoundData = {
-        start: Math.floor(Date.now()/1000),
-        end: Math.floor(Date.now()/1000) + 1,
-        reviewPeriodDuration: 1e9,
-        bounty: web3.toWei(5)
-      }
-    const tournamentThree = await createTournament(web3.toWei(10), scThree_RoundData, 0)
-    let scFour_RoundData = {
-        start: Math.floor(Date.now()/1000),
-        end: Math.floor(Date.now()/1000) + 1e9,
-        reviewPeriodDuration: 1,
-        bounty: web3.toWei(5)
-      }
-    const tournamentFour = await createTournament(web3.toWei(10), scFour_RoundData, 0)
-    await createSubmission(tournamentFour, 1)
-    await createSubmission(tournamentFour, 2)
-    await createSubmission(tournamentFour, 3)
-    let scFive_RoundData = {
-        start: Math.floor(Date.now()/1000),
-        end: Math.floor(Date.now()/1000) + 10,
-        reviewPeriodDuration: 1e9,
-        bounty: web3.toWei(5)
-      }
-    const tournamentFive = await createTournament(web3.toWei(10), scFive_RoundData, 0)
-    await createSubmission(tournamentFive, 1)
-    await createSubmission(tournamentFive, 2)
-    await createSubmission(tournamentFive, 3)
-    let scSix_RoundData = {
-        start: Math.floor(Date.now()/1000),
-        end: Math.floor(Date.now()/1000) + 10,
-        reviewPeriodDuration: 1,
-        bounty: web3.toWei(5)
-      }
-      const tournamentSix = await createTournament(web3.toWei(10), scSix_RoundData, 0)
-      await createSubmission(tournamentSix, 1);
-      await createSubmission(tournamentSix, 2);
-      await createSubmission(tournamentSix, 3);
-      let scSeven_RoundData = {
-        start: Math.floor(Date.now()/1000),
-        end: Math.floor(Date.now()/1000) + 10,
-        reviewPeriodDuration: 1e9,
-        bounty: web3.toWei(5)
-      }
-      let scSeven_RoundDataTwo = {
-        start: Math.floor(Date.now()/1000) + 30,
-        end: Math.floor(Date.now()/1000) + 1e9,
-        reviewPeriodDuration: 1e9,
-        bounty: web3.toWei(5)
-      }
-      const tournamentSeven = await createTournament(web3.toWei(10), scSeven_RoundData, 0)
-      await createSubmission(tournamentSeven, 1);
-      await createSubmission(tournamentSeven, 2);
-      await createSubmission(tournamentSeven, 3);
-      const tournamentSevenSubmissions = await getSubmissions(tournamentSeven);
-      await selectWinnersWhenInReview(tournamentSeven, 0, tournamentSevenSubmissions, tournamentSevenSubmissions.map(s => "1"), [0,0,0,0], 0)
-    // await getSubmissions(tournament)
+    const tournament_b = await createTournament(web3.toWei(10), RoundUnfunded, tournamentCreator)
+
+    // ----------------------------------------------------------------------------------------------
+
+    let RoundOpenWithZero = {
+      start: Math.floor(Date.now() / 1000),
+      end: Math.floor(Date.now() / 1000) + 9999999,
+      reviewPeriodDuration: 12345678,
+      bounty: web3.toWei(3),
+      closed: false
+    }
+    const tournament_c = await createTournament(web3.toWei(10), RoundOpenWithZero, tournamentCreator)
+
+    // ----------------------------------------------------------------------------------------------
+
+    let RoundOpenWithThree = {
+      start: Math.floor(Date.now() / 1000),
+      end: Math.floor(Date.now() / 1000) + 9999999,
+      reviewPeriodDuration: 12345678,
+      bounty: web3.toWei(3),
+      closed: false
+    }
+    const tournament_d = await createTournament(web3.toWei(10), RoundOpenWithThree, tournamentCreator)
+    submission = await createSubmission(tournament_d, 1)
+    await updateSubmission(submission)
+    await createSubmission(tournament_d, 2)
+    await createSubmission(tournament_d, 3)
+
+    // ----------------------------------------------------------------------------------------------
+
+    let RoundHasWinnersWithThree = {
+      start: Math.floor(Date.now() / 1000),
+      end: Math.floor(Date.now() / 1000) + 10,
+      reviewPeriodDuration: 12345678,
+      bounty: web3.toWei(3),
+      closed: false
+    }
+    const tournament_e = await createTournament(web3.toWei(10), RoundHasWinnersWithThree, tournamentCreator)
+    await createSubmission(tournament_e, 1)
+    await createSubmission(tournament_e, 2)
+    await createSubmission(tournament_e, 3)
+    submissions_e = await logSubmissions(tournament_e)
+    await selectWinnersWhenInReview(tournament_e, tournamentCreator, submissions_e, submissions_e.map(s => 1), RoundHasWinnersWithThree, 0)
+
+    // ----------------------------------------------------------------------------------------------
+
+    let RoundInReviewWithZero = {
+      start: Math.floor(Date.now() / 1000),
+      end: Math.floor(Date.now() / 1000) + 10, // CHANGE FOR ROPSTEN
+      reviewPeriodDuration: 9999999,
+      bounty: web3.toWei(3),
+      closed: false
+    }
+    const tournament_f = await createTournament(web3.toWei(10), RoundInReviewWithZero, tournamentCreator)
+
+    // ----------------------------------------------------------------------------------------------
+
+    let RoundInReviewWithThree = {
+      start: Math.floor(Date.now() / 1000),
+      end: Math.floor(Date.now() / 1000) + 10, // CHANGE FOR ROPSTEN
+      reviewPeriodDuration: 9999999,
+      bounty: web3.toWei(3),
+      closed: false
+    }
+    const tournament_g = await createTournament(web3.toWei(10), RoundInReviewWithThree, tournamentCreator)
+    await createSubmission(tournament_g, 1)
+    await createSubmission(tournament_g, 2)
+    await createSubmission(tournament_g, 3)
+    submissions_g = await logSubmissions(tournament_g)
+    await selectWinnersWhenInReview(tournament_g, tournamentCreator, submissions_g, submissions_g.map(s => 1), RoundHasWinnersWithThree, 0)
+
+    // ----------------------------------------------------------------------------------------------
+
+    let RoundClosedWithThree = {
+      start: Math.floor(Date.now() / 1000),
+      end: Math.floor(Date.now() / 1000) + 10, // CHANGE FOR ROPSTEN
+      reviewPeriodDuration: 120,
+      bounty: web3.toWei(3),
+      closed: false
+    }
+    const tournament_h = await createTournament(web3.toWei(10), RoundClosedWithThree, tournamentCreator)
+    await createSubmission(tournament_h, 1)
+    await createSubmission(tournament_h, 2)
+    await createSubmission(tournament_h, 3)
+    submissions_h = await logSubmissions(tournament_h)
+    await selectWinnersWhenInReview(tournament_h, tournamentCreator, submissions_h, submissions_h.map(s => 1), RoundHasWinnersWithThree, 0)
+
+    // ----------------------------------------------------------------------------------------------
+
+    let RoundAbandonedWithZero = {
+      start: Math.floor(Date.now() / 1000),
+      end: Math.floor(Date.now() / 1000) + 1,
+      reviewPeriodDuration: 1,
+      bounty: web3.toWei(3),
+      closed: false
+    }
+    const tournament_i = await createTournament(web3.toWei(10), RoundAbandonedWithZero, tournamentCreator)
+
+    // ----------------------------------------------------------------------------------------------
+
+    let RoundAbandonedWithThree = {
+      start: Math.floor(Date.now() / 1000),
+      end: Math.floor(Date.now() / 1000) + 30, // CHANGE FOR ROPSTEN
+      reviewPeriodDuration: 1,
+      bounty: web3.toWei(3),
+      closed: false
+    }
+    const tournament_j = await createTournament(web3.toWei(10), RoundAbandonedWithThree, tournamentCreator)
+    await createSubmission(tournament_j, 1)
+    await createSubmission(tournament_j, 2)
+    await createSubmission(tournament_j, 3)
   } catch (err) {
     console.log(err.message)
   } finally {
+    timeouts.forEach(t => clearTimeout(t))
     exit()
   }
 }
