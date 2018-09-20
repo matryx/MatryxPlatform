3fb0f62b898c7c3b89e5d0de6f6a7c0d6773523e
Alex McNerney <ajm13@me.com>
2018-09-20 10:38:59 -0700

    changed createRound to internal

--- contracts/MatryxTournament.sol
+++ contracts/MatryxTournament.sol
@@ -33,7 +33,6 @@ interface IMatryxTournament {
     function getDetails() external view returns (LibTournament.TournamentDetails);
 
     function enterTournament() external;
-    function createRound(LibRound.RoundDetails) external returns (address);
     function createSubmission(LibSubmission.SubmissionDetails) external returns (address);
 
     function selectWinners(LibRound.WinnersData, LibRound.RoundDetails) external;
@@ -135,7 +134,7 @@ library LibTournament {
     /// @param data      Data struct on Platform
     /// @param rDetails  Details of the Round being created
     /// @return          Address of the created Round
-    function createRound(address self, address sender, MatryxPlatform.Info storage info, MatryxPlatform.Data storage data, LibRound.RoundDetails rDetails) public returns (address) {
+    function createRound(address self, address sender, MatryxPlatform.Info storage info, MatryxPlatform.Data storage data, LibRound.RoundDetails rDetails) internal returns (address) {
         LibTournament.TournamentData storage tournament = data.tournaments[self];
         require(sender == tournament.owner, "Must be owner");
         require(IMatryxToken(info.token).balanceOf(self) >= rDetails.bounty, "Insufficient funds for Round");
