ba03c264bc97b6937dc19b9e7cabab0d93252558
Alex McNerney <ajm13@me.com>
2018-10-05 16:43:37 -0700

    Added overwrite false to deploys, must delete artifact before migrate

--- migrations/4_migrate_user.js
+++ migrations/4_migrate_user.js
@@ -3,5 +3,5 @@ var MatryxUser = artifacts.require('MatryxUser')
 var network = require('../truffle/network')
 
 module.exports = function (deployer) {
-  deployer.deploy(MatryxUser, '1', MatryxSystem.address)
+  deployer.deploy(MatryxUser, '1', MatryxSystem.address, { overwrite: false })
 }
--- migrations/5_migrate_libraries.js
+++ migrations/5_migrate_libraries.js
@@ -7,19 +7,19 @@ var LibSubmission = artifacts.require('LibSubmission')
 var LibTrinity = artifacts.require('LibTrinity')
 
 module.exports = function (deployer) {
-  deployer.deploy(LibUtils)
-  deployer.deploy(LibUser)
+  deployer.deploy(LibUtils, { overwrite: false })
+  deployer.deploy(LibUser, { overwrite: false })
 
-  deployer.deploy(LibTrinity)
+  deployer.deploy(LibTrinity, { overwrite: false })
   deployer.link(LibTrinity, LibPlatform)
   deployer.link(LibTrinity, LibTournament)
   deployer.link(LibTrinity, LibRound)
   deployer.link(LibTrinity, LibSubmission)
 
-  deployer.deploy(LibTournament)
-  deployer.deploy(LibRound)
-  deployer.deploy(LibSubmission)
+  deployer.deploy(LibTournament, { overwrite: false })
+  deployer.deploy(LibRound, { overwrite: false })
+  deployer.deploy(LibSubmission, { overwrite: false })
 
   deployer.link(LibTournament, LibPlatform)
-  deployer.deploy(LibPlatform)
+  deployer.deploy(LibPlatform, { overwrite: false })
 }
