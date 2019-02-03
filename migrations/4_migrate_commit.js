03d0979fe17b347b1df9c98367558e1eeb625133
Max Howard <max@nanome.ai>
2019-02-02 16:07:59 -0800

    Migrating commit separately due to gas errors

---
+++ migrations/4_migrate_commit.js
@@ -0,0 +1,7 @@
+const MatryxSystem = artifacts.require('MatryxSystem')
+const MatryxCommit = artifacts.require('MatryxCommit')
+const network = require('../truffle/network')
+
+module.exports = function(deployer) {
+  deployer.deploy(MatryxCommit, '1', MatryxSystem.address, { gas: 8e6, overwrite: false })
+}
