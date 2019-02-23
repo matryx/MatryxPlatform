05ebe3b36e99e02f7dd44330e0f5a0b012929824
Marina Torras <mtorras@ucsd.edu>
2019-02-22 16:33:59 -0800

    Combined LibTournament and LibTournamentHelper, cleaned up migrations

--- migrations/5_migrate_libraries.js
+++ migrations/5_migrate_libraries.js
@@ -1,19 +1,13 @@
 const LibPlatform = artifacts.require('LibPlatform')
 const LibCommit = artifacts.require('LibCommit')
 const LibTournament = artifacts.require('LibTournament')
-const LibTournamentHelper = artifacts.require('LibTournamentHelper')
-
-const network = require('../truffle/network')
 
 module.exports = function (deployer) {
-  deployer.deploy(LibTournamentHelper, { gas: 7e6, overwrite: false })
-  deployer.link(LibTournamentHelper, LibTournament)
-  
   deployer.deploy(LibTournament, { gas: 7e6, overwrite: false })
   deployer.link(LibTournament, LibPlatform)
 
   deployer.deploy(LibPlatform, { gas: 7e6, overwrite: false })
-  
+
   deployer.link(LibTournament, LibCommit)
   deployer.deploy(LibCommit, { gas: 7e6, overwrite: false })
 }
