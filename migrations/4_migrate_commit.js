05f05a9b2fdb2cd067b2aad5b2ba750f46f6f336
Max Howard <max@nanome.ai>
2019-02-26 11:38:29 -0800

    deleted redundant commit migration file, renamed migration files above

--- migrations/4_migrate_commit.js
+++
@@ -1,6 +0,0 @@
-const MatryxSystem = artifacts.require('MatryxSystem')
-const MatryxCommit = artifacts.require('MatryxCommit')
-
-module.exports = function(deployer) {
-  deployer.deploy(MatryxCommit, '1', MatryxSystem.address, { gas: 7e6, overwrite: false })
-}
